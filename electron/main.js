18557215c6d214fd8ef5aa926cf1d5bfe6b3fb71
Muhammed Irfan Kamaludeen <i7326@live.com>
2017-08-24 03:11:33 +0530

    Misc

--- electron/main.js
+++ electron/main.js
@@ -10,7 +10,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 640, height: 480})
+  mainWindow = new BrowserWindow({width: 640, height: 480, resizable: false, fullscreen: false})
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
--- www/main.js
+++ www/main.js
@@ -11,7 +11,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 640, height: 480})
+  mainWindow = new BrowserWindow({width: 640, height: 480, resizable: false, fullscreen: false})
   // and load the index.html of the app.
   //mainWindow.loadURL(`file://${__dirname}/index.html`)
   mainWindow.loadURL(`http://localhost:4200`)
