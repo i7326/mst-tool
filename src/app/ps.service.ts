6066d02ff39930fb69e7ebd4368754cd8a5a5ed3
Muhammed Irfan Kamaludeen <i7326@live.com>
2017-08-25 11:54:27 +0530

    Temp file management intial

--- src/app/ps.service.ts
+++ src/app/ps.service.ts
@@ -3,25 +3,42 @@ import { Shell } from 'node-powershell';
 import { Observable } from 'rxjs/Observable';
 import { remote } from 'electron';
 import { join } from 'path';
-import { readFileSync } from 'fs';
+import { env } from 'process';
+import { open } from 'temp-fs';
+import { createWriteStream, createReadStream } from 'fs';
 import { LoaderService } from './loader/loader.service';
 import 'rxjs/add/observable/fromPromise';
 import 'rxjs/Rx';
 
 @Injectable()
 export class PSService {
-  private _scriptdir = remote.app.getAppPath();
-  public _shell: Shell = new Shell({
+  private scriptDir = remote.app.getAppPath();
+  private script:string[];
+  public shell: Shell = new Shell({
     executionPolicy: 'Bypass',
     noProfile: true
   });
-  constructor(private loaderService: LoaderService, private zone: NgZone) { }
+  constructor(private loaderService: LoaderService, private zone: NgZone) {
+    let script:string[];
+    open({suffix:'.ps1'},function (err, file) {
+    if (err) { throw err; }
+      createReadStream(`${join(remote.app.getAppPath(), 'scripts', 'generate-mst.ps1')}`).pipe(createWriteStream(join(file.path)));
+      script['generate-mst'] = file;
+    });
+    console.log(script);
+
+      //var tmpobj = dirSync();
+    //console.log(platform());
+// Manual cleanup
+      //tmpobj.removeCallback();
+    createReadStream(`${join(this.scriptDir, 'scripts', 'generate-mst.ps1')}`).pipe(createWriteStream(join(env.TMP,'newLog.log')));
+  }
 
   run(script, param): Observable<any> {
     let shellOutput;
     this.zone.run(() => { this.showLoader() });
-    this._shell.addCommand(`&"${join(this._scriptdir, 'scripts', script)}"`, param);
-    return Observable.fromPromise( this._shell.invoke())
+    this.shell.addCommand(`&"${join(this.scriptDir, 'scripts', script)}"`, param);
+    return Observable.fromPromise( this.shell.invoke())
       .map( data => data )
       .catch(this.handleError)
       .finally(() => {
@@ -36,7 +53,7 @@ export class PSService {
     let errMsg = (error.message) ? error.message :
       error.status ? `${error.status} - ${error.statusText}` : 'Command error';
     console.error(errMsg); // log to console instead
-    this._shell.dispose();
+    this.shell.dispose();
     return Observable.throw(errMsg);
   }
 
@@ -46,5 +63,9 @@ export class PSService {
 
   private hideLoader(): void {
     this.loaderService.hide();
+  }
+
+  ngOnDestroy() {
+    console.log('destroyed');
   }
 }
